94ae332c1f424d81e070050dabf34eecca781b68
sousousore1 <souichi.saitou@gmail.com>
2018-07-06 17:12:34 +0900

    Add direct solution

--- lib/rvincenty.rb
+++ lib/rvincenty.rb
@@ -66,6 +66,57 @@ module RVincenty
     return s;
   end
 
+  # Destination given distance & bearing from start point (direct solution)
+  def self.direct(point_a, bearing, distance)
+    a = 6378137.0
+    b = 6356752.31414036
+    f = 1 / 298.257222101
+
+    lat1, lng1 = point_a
+    az1 = bearing * Math::PI / 180.0
+    sin_az1 = Math.sin(az1)
+    cos_az1 = Math.cos(az1)
+
+    u1 = Math.atan((1 - f) * Math.tan(lat1 * Math::PI / 180.0))
+    sigma1 = Math.atan2((1 - f) * Math.tan(lat1 * Math::PI / 180.0), cos_az1)
+
+    sin_u1 = Math.sin(u1)
+    cos_u1 = Math.cos(u1)
+    sin_alfa = cos_u1 * sin_az1
+    cos_alfa2 = 1 - sin_alfa ** 2
+
+    uu = cos_alfa2 * (a * a - b * b) / (b * b)
+    a1 = 1 + (uu / 16384.0) * (4096 + uu * (-768 + uu * (320 - 175 * uu)))
+    b1 = (uu / 1024.0) * (256 + uu * (-128 + uu * (74 - 47 * uu)))
+
+    iter_limit = 100
+    sigma = distance / (b * a1)
+    loop do
+      cos2sigmam = Math.cos(2 * sigma1 + sigma)
+      sin_sigma = Math.sin(sigma)
+      cos_sigma = Math.cos(sigma)
+      d_sigma = b1 * sin_sigma * (cos2sigmam + (b1 / 4.0) * (cos_sigma * (-1 + 2 * cos2sigmam ** 2))) - (b1 / 6.0) * cos2sigmam * (-3 + 4 * sin_sigma ** 2) * (-3 + 4 * cos2sigmam ** 2)
+      sigma0 = sigma
+      sigma = distance / (b * a1) + d_sigma
+      iter_limit -= 1
+      break if (sigma - sigma0).abs <= 1.0e-12 || iter_limit <= 0
+    end
+
+    cos2sigmam = Math.cos(2 * sigma1 + sigma)
+    sin_sigma = Math.sin(sigma)
+    cos_sigma = Math.cos(sigma)
+
+    phi2 = Math.atan2(sin_u1 * cos_sigma + cos_u1 * sin_sigma * cos_az1, (1 - f) * Math.sqrt(sin_alfa * sin_alfa + (sin_u1 * sin_sigma - cos_u1 * cos_sigma * cos_az1) ** 2))
+    lamuda = Math.atan2(sin_sigma * sin_az1, cos_u1 * cos_sigma - sin_u1 * sin_sigma * cos_az1)
+    c1 = (f / 16.0) * cos_alfa2 * (4 + f * (4 - 3 * cos_alfa2))
+    omega = lamuda - (1 - c1) * f * sin_alfa * (sigma + c1 * sin_sigma * (cos2sigmam + c1 * cos_sigma * (-1 + 2 * cos2sigmam ** 2)))
+    lamuda2 = lng1 * Math::PI / 180.0 + omega
+
+    lat = phi2 * 180 / Math::PI
+    lng = lamuda2 * 180 / Math::PI
+    [lat, lng]
+  end
+
   private
 
   def self.deg_to_rad(deg)
